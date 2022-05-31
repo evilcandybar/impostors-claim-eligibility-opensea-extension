7e5d38ee6608c7a8dd3976f78069e67671e75506
David Williams <david.williams@bjss.com>
2022-05-31 10:45:46 +0100

    Fixed bug - plguin should work consistently now - no refresh required

--- release/background.js
+++ release/background.js
@@ -1,7 +1,11 @@
-chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-
-    chrome.scripting.executeScript({
-        target: {tabId: details.tabId, allFrames: true},
-        files: ['content-script.js'],
-    });
-});
+chrome.tabs.onUpdated.addListener(function
+    (tabId, changeInfo, tab) {
+      if (changeInfo.url) {
+        setTimeout(function(){
+        chrome.scripting.executeScript({
+            target: {tabId: tabId, allFrames: true},
+            files: ['content-script.js'],
+        })}, 1000);
+      }
+    }
+  );
